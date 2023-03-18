8d26fb615001b85ae21458d18b0ac37636250e18
hnucamendi <arriolaharold2001@gmail.com>
2023-03-18 18:20:27 -0400

    aboutMe

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import AboutMe from "./components/AboutMe/AboutMe";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <AboutMe></AboutMe>
       <Footer></Footer>
     </>
   );
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,10 +2,23 @@ import "../../styles/Footer/Footer.css";
 import ListItem from "../Shared/ListItem";
 
 const Footer: React.FC = () => {
+  const symbols = ["lambda"];
   return (
     <footer className="main-footer">
       <ol>
-        <ListItem title="Test" isLink={false}></ListItem>
+        <ListItem title="&lambda;hnucamendi" isLink={false}></ListItem>
+        <ListItem
+          title="&lambda;Linkedin"
+          link="https://www.linkedin.com/in/hnucamendi/"
+          isLink={true}
+          target="_blank"
+        ></ListItem>
+        <ListItem
+          title="&lambda;GitHub"
+          link="https://github.com/hnucamendi"
+          isLink={true}
+          target="_blank"
+        ></ListItem>
       </ol>
     </footer>
   );
